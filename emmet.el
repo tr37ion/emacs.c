baddccba725abbf6a2ec4b34f78fa62b9657ac15
tr37ion <tr37ion@madguru.de>
2015-07-29 21:21:24 +0200

    Modified Emmet indent and add hook to web-mode

--- emmet.el
+++ emmet.el
@@ -2,4 +2,6 @@
 (require 'emmet-mode)
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'web-mode-hook  'emmet-mode) ;; HTML with Emmet
 (setq emmet-move-cursor-between-quotes t) ;; default nil
+(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
