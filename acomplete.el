d4f7956f54d996233c33ff07ce985a38c79a877b
tr37ion <tr37ion@madguru.de>
2015-07-30 17:55:12 +0200

    Add some AC options.

--- acomplete.el
+++ acomplete.el
@@ -2,13 +2,13 @@
 (require 'auto-complete)
 (require 'auto-complete-config)
 (ac-config-default)
-;;(global-auto-complete-mode t)
+(global-auto-complete-mode t)
 ;;(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-;;(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
 ;; Start auto-completion after 2 characters of a word
-;;(setq ac-auto-start 2)
+(setq ac-auto-start 2)
 ;; case sensitivity is important when finding matches
-;;(setq ac-ignore-case nil)
+(setq ac-ignore-case nil)
 ;; Change default sources
 ;;(setq-default ac-sources '(ac-source-abbrev ac-source-words-in-buffer))
 ;;(ac-flyspell-workaround)
